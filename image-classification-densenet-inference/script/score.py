adcb2903de210dce7d380315a655dddb845d127b
Yuhang Zhao <t-yuhazh@microsoft.com>
2019-08-14 18:02:45 +0800

    Add script

---
+++ image-classification-densenet-inference/script/score.py
@@ -0,0 +1,114 @@
+import os
+import json
+import fire
+from PIL import Image
+from io import BytesIO
+import base64
+import pandas as pd
+import pyarrow.parquet as pq  # noqa: F401
+from azureml.studio.modulehost.handler.port_io_handler import OutputHandler
+from azureml.studio.common.datatypes import DataTypes
+from azureml.studio.common.datatable.data_table import DataTable
+
+import torch
+import torch.nn as nn
+from torchvision import transforms
+
+from .densenet import densenet201, densenet169, densenet161, densenet121
+
+
+class Score:
+    def __init__(self, model_path='saved_model', meta={}):
+        self.mean = [0.485, 0.456, 0.406]
+        self.stdv = [0.229, 0.224, 0.225]
+        self.inference_transforms = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=self.mean, std=self.stdv),
+        ])
+        if meta['Memory efficient'] == 'True':
+            self.memory_efficient = True
+        else:
+            self.memory_efficient = False
+        if meta['Model type'] == 'densenet201':
+            self.model = densenet201(pretrained=False, memory_efficient=self.memory_efficient)
+        elif meta['Model type'] == 'densenet169':
+            self.model = densenet169(pretrained=False, memory_efficient=self.memory_efficient)
+        elif meta['Model type'] == 'densenet161':
+            self.model = densenet161(pretrained=False, memory_efficient=self.memory_efficient)
+        else:
+            self.model = densenet121(pretrained=False, memory_efficient=self.memory_efficient)
+        self.model.load_state_dict(torch.load(os.path.join(model_path, 'model.pth'), map_location='cpu'))
+
+        if torch.cuda.is_available():
+            self.model = self.model.cuda()
+            if torch.cuda.device_count() > 1:
+                self.model = torch.nn.DataParallel(self.model).cuda()
+
+        self.model.eval()
+
+    def run(self, input, meta=None):
+        my_list = []
+        for i in range(input.shape[0]):
+            # temp_string = json.loads(input.iloc[i]['image_string'])
+            temp_string = input.iloc[i]['image_string']
+            if temp_string.startswith('data:'):
+                my_index = temp_string.find('base64,')
+                temp_string = temp_string[my_index + 7:]
+            temp = base64.b64decode(temp_string)
+            img = Image.open(BytesIO(temp))
+            input_tensor = self.inference_transforms(img)
+            input_tensor = input_tensor.unsqueeze(0)
+            if torch.cuda.is_available():
+                input_tensor = input_tensor.cuda()
+
+            with torch.no_grad():
+                output = self.model(input_tensor)
+                softmax = nn.Softmax(dim=1)
+                pred_probs = softmax(output).cpu().numpy()[0]
+                index = torch.argmax(output, 1)[0].cpu().item()
+
+            result = [index, str(pred_probs[index])]
+            my_list.append(result)
+
+        df = pd.DataFrame(my_list, columns=['index', 'probability'])
+        return df
+
+    def evaluate(self, data_path='test_data', save_path='outputs'):
+        os.makedirs(save_path, exist_ok=True)
+        input = pd.read_parquet(os.path.join(data_path, 'data.dataset.parquet'), engine='pyarrow')
+        df = self.run(input)
+        dt = DataTable(df)
+        OutputHandler.handle_output(data=dt, file_path=save_path,
+                                    file_name='data.dataset.parquet', data_type=DataTypes.DATASET)
+
+
+def test(model_path='script/saved_model', data_path='script/outputs', save_path='script/outputs2',
+         model_type='densenet201', memory_efficient=False):
+    meta = {'Model type': model_type, 'Memory efficient': str(memory_efficient)}
+    score = Score(model_path, meta)
+    score.evaluate(data_path=data_path, save_path=save_path)
+
+    # Dump data_type.json as a work around until SMT deploys
+    dct = {
+        'Id': 'Dataset',
+        'Name': 'Dataset .NET file',
+        'ShortName': 'Dataset',
+        'Description': 'A serialized DataTable supporting partial reads and writes',
+        'IsDirectory': False,
+        'Owner': 'Microsoft Corporation',
+        'FileExtension': 'dataset.parquet',
+        'ContentType': 'application/octet-stream',
+        'AllowUpload': False,
+        'AllowPromotion': True,
+        'AllowModelPromotion': False,
+        'AuxiliaryFileExtension': None,
+        'AuxiliaryContentType': None
+    }
+    with open(os.path.join(save_path, 'data_type.json'), 'w') as f:
+        json.dump(dct, f)
+
+
+if __name__ == '__main__':
+    fire.Fire(test)
