8a1082fe8f88e0d477d0d2e145ead6725ce1b384
Yuhang Zhao <t-yuhazh@microsoft.com>
2019-08-15 12:41:50 +0800

    Extract repeated smt fake code to a new file

--- semantic-segmentation-deeplabv3/script/train.py
+++ semantic-segmentation-deeplabv3/script/train.py
@@ -1,9 +1,9 @@
 import os
-import json
 import fire
 import torch
 
 from .model import deeplabv3_resnet101, fcn_resnet101
+from .smt_fake import smt_fake_model
 
 
 def entrance_fake(model_type='deeplabv3_resnet101', model_path='script/saved_model', save_path='script/saved_model'):
@@ -16,28 +16,8 @@ def entrance_fake(model_type='deeplabv3_resnet101', model_path='script/saved_mod
         model = fcn_resnet101(model_path=model_path, pretrained=True)
     torch.save(model.state_dict(), os.path.join(save_path, 'model.pth'))
 
-    # Dump data_type.json as a work around until SMT deploys
-    dct = {
-        'Id': 'ILearnerDotNet',
-        'Name': 'ILearner .NET file',
-        'ShortName': 'Model',
-        'Description': 'A .NET serialized ILearner',
-        'IsDirectory': False,
-        'Owner': 'Microsoft Corporation',
-        'FileExtension': 'ilearner',
-        'ContentType': 'application/octet-stream',
-        'AllowUpload': False,
-        'AllowPromotion': False,
-        'AllowModelPromotion': True,
-        'AuxiliaryFileExtension': None,
-        'AuxiliaryContentType': None
-    }
-    with open(os.path.join(save_path, 'data_type.json'), 'w') as f:
-        json.dump(dct, f)
-    # Dump data.ilearner as a work around until data type design
-    visualization = os.path.join(save_path, 'data.ilearner')
-    with open(visualization, 'w') as file:
-        file.writelines('{}')
+    smt_fake_model(save_path)
+
     print('This experiment has been completed.')
 
 
