e25de7b0dd0feb6c405e29b9c2040e28aaf40513
Yuhang Zhao <t-yuhazh@microsoft.com>
2019-09-11 15:27:25 +0800

    Add 'find_packages' function in setup.py

--- semantic-segmentation-deeplabv3/setup.py
+++ semantic-segmentation-deeplabv3/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='semantic-segmentation-deeplabv3',
     version='0.0.1',
     description='Deeplabv3 Model for Semantic Segmentation Task',
-    packages=['script'],
+    packages=find_packages(),
     author='Yuhang Zhao',
     license='MIT',
     include_package_data=True,
