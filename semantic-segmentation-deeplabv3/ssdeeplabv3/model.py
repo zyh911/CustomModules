2ed840364af62ff458bc919d11bc273ecfd77f7a
Yuhang Zhao <t-yuhazh@microsoft.com>
2019-09-13 15:26:17 +0800

    Delete backup import

--- semantic-segmentation-deeplabv3/ssdeeplabv3/model.py
+++ semantic-segmentation-deeplabv3/ssdeeplabv3/model.py
@@ -1,10 +1,7 @@
 import sys
 import torch.nn as nn
 from torchvision.models.segmentation.segmentation import _segm_resnet, model_urls
-try:
-    from torch.hub import load_state_dict_from_url
-except ImportError:
-    from torch.utils.model_zoo import load_url as load_state_dict_from_url
+from torch.hub import load_state_dict_from_url
 
 
 __all__ = ['SegmentationNet']
