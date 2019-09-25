ac8aee908675fef99970ab8523ef739d51d3ef5b
Yuhang Zhao <t-yuhazh@microsoft.com>
2019-09-25 14:08:48 +0800

    Upload spec and setup part

---
+++ reinforcement-learning-ddpg/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='reinforcement-learning-ddpg',
+    version='0.0.1',
+    description='DDPG Model for Reinforcement Learning Tasks',
+    packages=find_packages(),
+    author='Yuhang Zhao',
+    license='MIT',
+    include_package_data=True,
+)
